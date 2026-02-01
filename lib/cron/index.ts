3bc2fda1203b513313a5f31f57caba6a9b548ec6
Andrea Santu <andreasantu@gmail.com>
2026-02-01 17:26:39 +0100

    Updated cron rule

--- lib/cron/index.ts
+++ lib/cron/index.ts
@@ -3,9 +3,9 @@ import { Adapter } from "../adapter";
 
 console.log("[cron] init");
 
+// cron.schedule("* * * * *", async () => {
 // “At 11:00 on Monday, Tuesday, Thursday, and Friday.”
-// cron.schedule("0 11 * * 1,2,4,5", async () => {
-cron.schedule("* * * * *", async () => {
+cron.schedule("0 11 * * 1,2,4,5", async () => {
   try {
     console.log("[cron] start");
     const adapter = new Adapter();
